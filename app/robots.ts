ad0d57be459654c83119ef4897ae9030eda7c58f
OMueller-1976 <om@onetitel.de>
2026-06-14 20:15:28 +0200

    Fix robots.txt: Sitemap-URL auf ferienwohnung-in-der-vulkaneifel.de korrigiert

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.maarzeit-vulkaneifel.de/sitemap.xml",
+    sitemap: "https://ferienwohnung-in-der-vulkaneifel.de/sitemap.xml",
   };
 }
